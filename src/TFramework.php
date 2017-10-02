33b3bc212c1f93f6b767a479141dfb36aca6b3b1
Eliel de Paula <elieldepaula@gmail.com>
2017-10-02 00:43:50 -0300

    First commit

---
+++ src/TFramework.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace elieldepaula\tframework;
+
+/**
+ * Description of TFramework
+ *
+ * @author elieldepaula
+ */
+class TFramework {
+    
+    function __construct() {
+        // something...
+    }
+    
+    public function teste()
+    {
+        return "Ola mundo...";
+    }
+    
+}
